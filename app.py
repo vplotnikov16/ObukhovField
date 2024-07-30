b3c7f0ea2584746f3d1536668ab8388a31b5fef2
vplotnikov16 <vplotnikov16@yandex.ru>
2024-07-30 21:26:19 +0500

    SSL configuration

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def update_config():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False, ssl_context=('server-cert.pem', 'server-key.pem'))
